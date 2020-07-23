c254a6d7aecc9cc62a4a1fb0ed4397be5f8a2cf1
Paul M Edwards <pauledwards@gmail.com>
2020-07-22 21:33:08 -0700

    Implement stack using array storage structure

--- stack/stack.py
+++ stack/stack.py
@@ -4,6 +4,7 @@ return elements in Last In First Out order.
 
 1. Implement the Stack class using an array as the underlying storage structure.
    Make sure the Stack tests pass.
+   Done
 2. Re-implement the Stack class, this time using the linked list implementation
    as the underlying storage structure.
    Make sure the Stack tests pass.
@@ -13,13 +14,17 @@ return elements in Last In First Out order.
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        self.storage.append(value)
+        self.size += 1
 
     def pop(self):
-        pass
+        if self.size == 0: return None
+        v = self.storage.pop()
+        self.size -= 1
+        return v
