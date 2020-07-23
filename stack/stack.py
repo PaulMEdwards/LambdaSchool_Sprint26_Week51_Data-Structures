de1c9afa743046483222196a6ecc36144f405571
Paul M Edwards <pauledwards@gmail.com>
2020-07-22 21:59:45 -0700

    Refactor stack to use LinkedList storage structure

--- stack/stack.py
+++ stack/stack.py
@@ -8,23 +8,36 @@ return elements in Last In First Out order.
 2. Re-implement the Stack class, this time using the linked list implementation
    as the underlying storage structure.
    Make sure the Stack tests pass.
+   Done
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
+   LinkedList manages it's own size and has different methods/properties from array.
 """
+
+cur_dir = 'stack'
+import sys, pathlib
+if str(pathlib.Path.cwd()).endswith(cur_dir):
+    sys.path.insert(0, str(pathlib.Path.cwd().parent))
+if str(sys.path[0]).endswith(cur_dir):
+    sys.path.insert(0, str(pathlib.Path.cwd()))
+
+try: from singly_linked_list.singly_linked_list import LinkedList
+except ModuleNotFoundError: pass
+except ImportError: pass
+try: from singly_linked_list import LinkedList
+except ModuleNotFoundError: pass
+except ImportError: pass
+
 class Stack:
     def __init__(self):
-        self.size = 0
-        self.storage = []
+        self.storage = LinkedList()
 
     def __len__(self):
-        return self.size
+        return self.storage.length
 
     def push(self, value):
-        self.storage.append(value)
-        self.size += 1
+        self.storage.add_to_tail(value)
 
     def pop(self):
-        if self.size == 0: return None
-        v = self.storage.pop()
-        self.size -= 1
-        return v
+        if self.storage.length == 0: return None
+        return self.storage.remove_tail()
