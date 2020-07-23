f95b33c4be457a0a63d6a24f830bf5d50026658a
Paul M Edwards <pauledwards@gmail.com>
2020-07-22 21:28:08 -0700

    Refactor queue to use LinkedList storage structure

--- queue/queue.py
+++ queue/queue.py
@@ -8,33 +8,46 @@ return elements in First In First Out order.
 2. Re-implement the Queue class, this time using the linked list implementation
    as the underlying storage structure.
    Make sure the Queue tests pass.
+   Done
 3. What is the difference between using an array vs. a linked list when 
    implementing a Queue?
+   Quite a few differences... LinkedList manages it's own size and has different methods/properties from array.
    
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+cur_dir = 'queue'
+import sys, pathlib
+if str(pathlib.Path.cwd()).endswith(cur_dir):
+    sys.path.insert(0, str(pathlib.Path.cwd().parent))
+if str(sys.path[0]).endswith(cur_dir):
+    sys.path.insert(0, str(pathlib.Path.cwd()))
+
+try: from singly_linked_list.singly_linked_list import LinkedList
+except ModuleNotFoundError: pass
+except ImportError: pass
+try: from singly_linked_list import LinkedList
+except ModuleNotFoundError: pass
+except ImportError: pass
+
 class Queue:
     def __init__(self):
-        self.size = 0
-        self.storage = []
-    
+        self.storage = LinkedList()
+
     def __len__(self):
-        s = self.size
+        s = self.storage.length
         # print(f"Size: {s}")
         return s
 
     def enqueue(self, value):
         # print(f"Enqueue: {value}")
-        self.storage.insert(0, value)
-        self.size += 1
-        # print(f"Size: {self.size}")
+        self.storage.add_to_tail(value)
+        # print(f"Size: {self.storage.length}")
 
     def dequeue(self):
-        i = self.size - 1
-        # print(f"Dequeue: #{i}, len={self.size}")
-        if i < 0: return None
-        # print(f"Value: {self.storage[i]}")
-        v = self.storage.pop(i)
-        self.size -= 1
+        if self.storage.length == 0: return None
+        # print(f"Value: {self.storage.head.get_value()}")
+        v = self.storage.head.get_value()
+        self.storage.remove_head()
         return v
