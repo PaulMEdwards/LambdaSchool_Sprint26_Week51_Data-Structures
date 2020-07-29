824176b0ec37f3b926df21d0996e329c6c670cc8
Paul M Edwards <pauledwards@gmail.com>
2020-07-28 20:27:42 -0700

    Implement binary_search_tree: insert, contains, and get_max

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,27 +9,93 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+
+DEPTH = [10]
+
+def printUtil(node, gap):
+    if (node is None):
+        return
+    gap += DEPTH[0]
+    printUtil(node.right, gap)
+    print()
+    for i in range(DEPTH[0], gap):
+        print(end = " ")
+    print(f"{node.value} ({node.count})")
+    printUtil(node.left, gap)
+    print()
+
+def printValues(node, label = None):
+    if label: print(label)
+    printUtil(node, 0)
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
+        self.count = 1
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if value == self.value:
+            self.count += 1
+            # print(f"incremented count for value:\t{self.value} ({self.count})")
+        elif value <= self.value:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        elif value > self.value:
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+        # printValues(self, f"inserted:\t{value}")
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # compare target_value to cur_value
+            # 1. == return True
+            # 2. < go left
+            # 3. > go right
+            # 4. not found, return False
+        result = False
+        ripCord = False
+        n = self
+        while result != True and n is not None:
+            if target == n.value: result = True
+            elif target > n.value:
+                if n.right is not None:
+                    n = n.right
+                else:
+                    n = None
+            elif target < n.value:
+                if n.left is not None:
+                    n = n.left
+                else:
+                    n = None
+            else:
+                n = None
+        # printValues(self, f"contains {target}?\t{result}")
+        return result
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        # print("get_max")
+        n = self
+        while n is not None:
+            max = n.value
+            n = n.right
+        # print(f"max = {max}")
+        return max
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
+        pass
+
+    # STRETCH
+    def delete(self, value):
         pass
 
     # Part 2 -----------------------
@@ -63,23 +129,23 @@ class BSTNode:
 """
 This code is necessary for testing the `print` methods
 """
-bst = BinarySearchTree(1)
+# bst = BSTNode(1)
 
-bst.insert(8)
-bst.insert(5)
-bst.insert(7)
-bst.insert(6)
-bst.insert(3)
-bst.insert(4)
-bst.insert(2)
+# bst.insert(8)
+# bst.insert(5)
+# bst.insert(7)
+# bst.insert(6)
+# bst.insert(3)
+# bst.insert(4)
+# bst.insert(2)
 
-bst.bft_print()
-bst.dft_print()
+# bst.bft_print()
+# bst.dft_print()
 
-print("elegant methods")
-print("pre order")
-bst.pre_order_dft()
-print("in order")
-bst.in_order_dft()
-print("post order")
-bst.post_order_dft()  
+# print("elegant methods")
+# print("pre order")
+# bst.pre_order_dft()
+# # print("in order")
+# # bst.in_order_dft()
+# print("post order")
+# bst.post_order_dft()
